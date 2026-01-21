c6cee9e30a5068ccab3fc2f4c03675683904e706
CreSoftMob <afonsodsilvamendes@icloud.com>
2026-01-20 22:19:08 -0300

    Update default avatar URL in service worker
    okay

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 // 2. VERSÃO E LINKS (🚨 Troque o link abaixo pelo logo do seu app)
 const VERSION = 'v1.2.0'; 
 const LOGO_APP = 'https://cresoftmob.github.io/img/logo.jpeg'; 
-const DEFAULT_AVATAR = 'https://cresoftmob.github.io/img/logo.jpeg';
+const DEFAULT_AVATAR = 'https://cdn-icons-png.flaticon.com/128/18827/18827926.png';
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
