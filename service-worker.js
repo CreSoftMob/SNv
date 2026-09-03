f9e49620d19c6be878654af730b7e768364d392a
CreSoftMob <afonsodsilvamendes@outlook.com>
2026-09-03 09:58:10 -0300

    will be that

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-const VERSION = 'v7.0'; // Incrementado para forçar atualização
+const VERSION = 'v7.1'; // Incrementado para forçar atualização
 
 const BASE_URL = 'https://samenext.com.br/';
 const LOGO_PADRAO_APP = 'https://cdn-icons-png.flaticon.com/128/18827/18827925.png'; 
